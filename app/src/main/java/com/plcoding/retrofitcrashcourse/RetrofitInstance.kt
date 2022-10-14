464d082421449b43ccede951815fdf3c9a84e9a7
janakbista <bistajanak303@gmail.com>
2022-10-14 17:30:41 +0545

    Url Cacheing

--- app/src/main/java/com/plcoding/retrofitcrashcourse/RetrofitInstance.kt
+++ app/src/main/java/com/plcoding/retrofitcrashcourse/RetrofitInstance.kt
@@ -1,15 +1,44 @@
 package com.plcoding.retrofitcrashcourse
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitInstance {
+object RetrofitInstance:Application() {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(context.cacheDir, cacheSize)
 
+    val okHttpClient = OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            val context = null
+            request = if (context?.let { hasNetwork(it) }!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+            chain.proceed(request)
+        }
+        .build()
     val api: TodoApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(TodoApi::class.java)
+    }
+    fun hasNetwork(context: Context): Boolean? {
+        var isConnected: Boolean? = false // Initial Value
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected)
+            isConnected = true
+        return isConnected
     }
 }
